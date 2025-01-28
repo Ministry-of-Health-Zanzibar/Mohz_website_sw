0c0d1bdf1c3daec3e1c65d4b9bb79de2212b8062
iddyochu <iddyochu@gmail.com>
2025-01-28 12:47:32 +0300

    changes

--- src/app/temp/layout/footer/footer.component.ts
+++ src/app/temp/layout/footer/footer.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SiteLinkService } from '../../../services/site-links/site-link.service';
+import { ToastService } from '../../../services/toast/toast.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+  siteLink: any;
+
+  constructor(private siteLinkService:SiteLinkService, private toastService: ToastService){}
+
+  ngOnInit(): void {
+    this.getAllSiteLinks();
+      
+  }
+    email = 'info@mohz.go.tz'
+
+
+     public getAllSiteLinks(): void {
+        this.siteLinkService.getAllSitelinks().subscribe((response: any) => {
+            this.siteLink = response.data;
+            console.log(response.data);
+          },
+          (errorResponse: HttpErrorResponse) => {
+            console.log(errorResponse.error.message);
+          },
+        );
+      }
 
 }
